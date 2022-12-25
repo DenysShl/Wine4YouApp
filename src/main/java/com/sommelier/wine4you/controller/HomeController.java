bae2dcac4df55d693faebc655428a49dc090c088
Denys.Sh <7860denis@gmail.com>
2022-12-25 20:17:43 +0200

    fixed checkstyle v4

--- src/main/java/com/sommelier/wine4you/controller/HomeController.java
+++ src/main/java/com/sommelier/wine4you/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v2/home")
+@RequestMapping("/api/v1/home")
 public class HomeController {
     @GetMapping
     public String getStart() {
